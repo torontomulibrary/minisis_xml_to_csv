6b40165efb521fc083d3eb45878935fa50076987
MJ Suhonos <mj@suhonos.ca>
2015-06-23 13:10:59 -0400

    Correct patch matching

--- xml_to_csv/preprocess_xml.rb
+++ xml_to_csv/preprocess_xml.rb
@@ -6,14 +6,15 @@ puts "Begin preprocessing input XML ..."
 total_elapsed = Benchmark.realtime do 
 	doc = Nokogiri::XML(File.open(PATH_TO_XML), nil, 'ISO-8859-1')
 
-	doc.xpath("//XML_RECORD/LOG_RECORD").remove
-	doc.xpath("//XML_RECORD/AUTH_MOD_HIST").remove
-	doc.xpath("//XML_RECORD/MODIFIED_HIST").remove
+  records = doc.xpath('//XML_RECORD')
+
+  records.each do |record|
+  	record.xpath("LOG_RECORD | AUTH_MOD_HIST | MODIFIED_HIST").remove
+  end
+
+  puts "Processed #{records.count} record nodes."
 
 	# We aren't using these, so we can get rid of them in the pre-process
-	doc.xpath("//XML_RECORD/LOWER_LEVEL").remove
-	doc.xpath("//XML_RECORD/ACCESSION_GRP").remove
-	doc.xpath("//XML_RECORD/ORIGINATION_GRP").remove
 
 	# ox doesnt seem to like the <?xml version="1.0" encoding="UTF-8"?> line before the data
 	File.open('preprocessed.xml', 'wb') { |f| f.print(doc.to_xml(:encoding => 'UTF-8', :save_with => Nokogiri::XML::Node::SaveOptions::NO_DECLARATION)) }
