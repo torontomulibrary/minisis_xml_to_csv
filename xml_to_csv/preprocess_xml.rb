88c0004f94de29031c5542eefbfe3dcf11214d55
Patrick Fung <patrick@makestuffdostuff.com>
2015-06-23 10:48:32 -0400

    update preproccess_xml.rb to remove some more elements that we dont use in our mapping

--- xml_to_csv/preprocess_xml.rb
+++ xml_to_csv/preprocess_xml.rb
@@ -10,6 +10,11 @@ total_elapsed = Benchmark.realtime do
 	doc.xpath("//XML_RECORD/AUTH_MOD_HIST").remove
 	doc.xpath("//XML_RECORD/MODIFIED_HIST").remove
 
+	# We aren't using these, so we can get rid of them in the pre-process
+	doc.xpath("//XML_RECORD/LOWER_LEVEL").remove
+	doc.xpath("//XML_RECORD/ACCESSION_GRP").remove
+	doc.xpath("//XML_RECORD/ORIGINATION_GRP").remove
+
 	# ox doesnt seem to like the <?xml version="1.0" encoding="UTF-8"?> line before the data
 	File.open('preprocessed.xml', 'wb') { |f| f.print(doc.to_xml(:encoding => 'UTF-8', :save_with => Nokogiri::XML::Node::SaveOptions::NO_DECLARATION)) }
 end
