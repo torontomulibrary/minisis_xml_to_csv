4414e920067b0252b2de6ca7ec24eea9da8c0fb3
MJ Suhonos <mj@suhonos.ca>
2015-06-24 14:24:41 -0400

    Add record mapping classes

--- xml_to_csv/lib/accession.rb
+++ xml_to_csv/lib/accession.rb
@@ -1,3 +1,35 @@
 class Accession
   include SAXMachine
+
+  # Used to generate an accessions CSV file
+  element :EXTENT,            as: :receivedExtentUnits
+  element :ACCNO,             as: :accessionNumber
+  element :ACC_TITLE,         as: :title
+  element :RECDATE,           as: :acquisitionDate
+  element :ACQUISITION_TYPE,  as: :acquisitionType
+  element :R_STATUS,          as: :processingStatus
+  element :ACC_SCOPE,         as: :scopeAndContent
+
+  # NB: this will take the value from all sub-elements, eg. ORGANIZATION | INDIVIDUAL
+  element :DONOR_GROUP,  as: :creators # FIXME: pipe-delimited
+  element :ACC_CREATOR,  as: :creators # FIXME: handle merging
+
+  element :EX_ACC_GROUP,  as: :PLEASE_FIX # FIXME: sub-elements: EX_ACC_DATE | EX_ACC_NOTES
+
+  # NB: this will take the value from all sub-elements, eg. LOCATION_DETAILS | ?
+  element :LOCATION_GROUP, as: :locationInformation # FIXME
+  element :BUS_UNIT_OWNER, as: :locationInformation # FIXME: handle merging
+
+  element :EXTENT_KEPT,       as: :processingNotes # FIXME: handle merging
+  element :COMMENTS_ACC,      as: :processingNotes # FIXME: handle merging
+  element :PROC_STATUS,       as: :processingNotes # FIXME: handle merging
+  element :ARRANGEMENT_NOTES, as: :processingNotes # FIXME: handle merging
+  element :PROCESSING_NOTES,  as: :processingNotes # FIXME: handle merging
+  element :ARC_NOTES,         as: :processingNotes # FIXME: handle merging
+
+  # NB: this will take the value from all sub-elements ?
+  element :VALUATION_GROUP, as: :appraisal
+
+  # NB: this will take the value from all sub-elements ?
+  element :DESPATCH_GRP, as: :processingNotes # FIXME: handle merging
 end
--- xml_to_csv/lib/authority.rb
+++ xml_to_csv/lib/authority.rb
@@ -2,15 +2,17 @@ class Authority
   include SAXMachine
 
   # Used to generate an authorities CSV file
-  element :AUTHORITY_TYPE
-  element :HEADING
-  element :ADMIN_HISTORY
-  element :STATUSA
-  element :CONTROLLING_GRP # FIXME: sub-element(s)
+  element :AUTHORITY_TYPE,  as: :typeOfEntity
+  element :HEADING,         as: :authorizedFormOfName
+  element :ADMIN_HISTORY,   as: :history
+  element :STATUSA,         as: :status
+
+  # NB: this will take the value from all sub-elements, eg. DATE_CONTROLLED
+  element :CONTROLLING_GRP, as: :datesOfExistence
 
   # Used to generate an alternate authorities CSV file
-  element :VARIANT_NAME
-  element :PREDECESSOR
-  element :SUCCESSOR
-  element :PARALLEL_NAME
+  element :VARIANT_NAME,    as: :alternateForm1
+  element :PREDECESSOR,     as: :alternateForm2
+  element :SUCCESSOR,       as: :alternateForm3
+  element :PARALLEL_NAME,   as: :alternateForm4
 end
