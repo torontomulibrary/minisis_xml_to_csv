c6f956c35656bc930920d6f3959304be4152014f
Patrick Fung <patrick@makestuffdostuff.com>
2015-07-06 15:48:01 -0400

    trying to figure out where data goes

--- xml_to_csv/descriptions_sort.rb
+++ xml_to_csv/descriptions_sort.rb
@@ -5,33 +5,41 @@ def sort_it(records, parents)
 
   ids = parents.map { |r| r[:legacyid] }
 
-  next_level = []
-  records.each do |r|
-    next_level << r if ids.include?(r[:parentid])
-  end
+  next_level = records.select { |r| ids.include?(r[:parentid]) }
 
   records = records - next_level
-
+  puts "Records remaining: #{records.count}"
   sort_it(records, next_level).unshift(*next_level)
 end
 
 CSV::Converters[:blank_to_nil] = lambda do |field|
-  field && field.empty? ? nil : field
+  field && field =~ /^\s*$/ ? nil : field
 end
 
-csv = CSV.new( File.open('./private_data/descriptions.xml.csv'), :headers => true, :header_converters => :symbol, :converters => [:all, :blank_to_nil])
+csv = CSV.new( File.open('./private_data/descriptions.nodup.noorphan.csv'), :headers => true, :header_converters => :symbol, :converters => [:all, :blank_to_nil])
 
 records = csv.to_a.map { |row| row.to_hash }
-roots = []
+puts "Total records: #{records.count}"
 
-records.each do |r|
-  roots << r if r[:parentid].nil?
-end
+roots = records.select { |r| r[:parentid].nil? }
+puts "Found roots: #{roots.count}"
 
-records = records - roots
-sorted = sort_it(records, roots)
+more = records - roots
+puts "Records remaining: #{records.count}"
+
+
+sorted = sort_it(more, roots)
 
 sorted.unshift(*roots)
+puts "Sorted array: #{sorted.count}"
+
+remain = records - sorted
+puts "Not sorted?.. : #{remain.count}"
+
+csv_sorted = CSV.open("./private_data/descriptions.what.csv", 'wb') do |f|
+  f << remain[1].keys
+  remain.each { |r| f << r.values }
+end
 
 csv_sorted = CSV.open("./private_data/descriptions.sorted.csv", 'wb') do |f|
   f << sorted[1].keys
